d0bab2e0938dc8f6d7cec291cfe41c98ba3397cd
ahabchi <amine.habchi.01@gmail.com>
2024-01-09 11:06:53 +0000

    test 10000

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq -r .[$HERO_ID-1].connections.relatives
+ curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq -r  ".[] | select(.id ==$HERO_ID) | .connections.relatives" 
