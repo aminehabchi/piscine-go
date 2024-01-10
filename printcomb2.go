f01661650a769eb64713bb3495611abab96664db
ahabchi <amine.habchi.01@gmail.com>
2024-01-10 11:13:38 +0000

    jghhujj

--- printcomb2.go
+++ printcomb2.go
@@ -6,12 +6,12 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			d := j + 1
-			for k := i; k <= '9'; k++ {
+			for a := i; a <= '9'; a++ {
 				for ; d <= '9'; d++ {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
-					z01.PrintRune(k)
+					z01.PrintRune(a)
 					z01.PrintRune(d)
 					if i < '9' || j < '8' {
 						z01.PrintRune(',')
