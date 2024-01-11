5304425bd66754b4bde7312b78b7e8541e16cebb
Christopher  Fremond <dev@01-edu.org>
2024-01-11 12:18:43 +0000

    Update 'strlen.go'

--- strlen.go
+++ strlen.go
@@ -2,6 +2,21 @@ package piscine
 
 func StrLen(s string) int {
 	b := []byte(s)
+	var a int
+	var c int = 0
+	for i := 0; i < len(b); i++ {
+		if b[i] <= 255 && b[i] >= 128 {
+			if c == 1 {
+				a++
+				c = 0
+			} else if c == 0 {
+				c++
+			}
+		}
 
-	return len(b)
+		if b[i] < 128 {
+			a++
+		}
+	}
+	return a
 }
