7f9afed6ec06f0b25d1a7f14838541afa6ce83f3
Christopher  Fremond <dev@01-edu.org>
2024-01-11 16:18:27 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -1,14 +1,13 @@
 package piscine
-
 func Atoi(s string) int {
 	sum := 0
 	sign := 1
 	for index, num := range s {
 
-		if num == '-' && index == len(s) {
+		if num == '-' && index == 0 {
 			sign = sign * -1
 			continue
-		} else if num == '+' && index == len(s) {
+		} else if num == '+' && index == 0 {
 			sign = sign * 1
 			continue
 		}
