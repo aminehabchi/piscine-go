6ad45c4c804b368e82bdcc6369d121fc35789d26
ahabchi <amine.habchi.01@gmail.com>
2024-01-14 10:57:02 +0000

    aa

--- sortintegertable.go
+++ sortintegertable.go
@@ -4,9 +4,9 @@ func SortIntegerTable(table []int) {
 	i := 1
 	for i < len(table) {
 		if table[i-1] > table[i] {
-			swap := table[i-1]
+			swp := table[i-1]
 			table[i-1] = table[i]
-			table[i] = swap
+			table[i] = swp
 			i = 1
 		} else {
 			i++
