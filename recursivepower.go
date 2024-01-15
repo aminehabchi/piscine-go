2014b6b725f2c0df69023668a04a7da8f1c8f174
ahabchi <you@example.com>
2024-01-15 10:44:55 +0000

    gh

---
+++ recursivepower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+	if nb < 0 {
+		return 0
+	}
+	result := 1
+	if power > 0 {
+		power--
+		result = nb * RecursivePower(nb, power)
+	}
+	return result
+}
