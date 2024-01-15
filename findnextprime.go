13162f0117519f273b96ca569fe85a58125eca52
ahabchi <you@example.com>
2024-01-15 12:33:49 +0000

    yjfv

---
+++ findnextprime.go
@@ -0,0 +1,26 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb == 1 || nb == 2 {
+		return 2
+	}
+
+	var b bool
+	var c float64
+	for b == false {
+		for i := 2; i < nb; i++ {
+			c = float64(nb) / float64(i)
+			if c == float64(int(c)) {
+				b = false
+				break
+			}
+			if c != float64(int(c)) {
+				b = true
+			}
+		}
+		if b == false {
+			nb++
+		}
+	}
+	return nb
+}
