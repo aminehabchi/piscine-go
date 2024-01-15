8b525167d5b73e88cb98cd0bd2836baa3da9f0f3
ahabchi <you@example.com>
2024-01-15 11:34:43 +0000

    ugv

--- isprime.go
+++ isprime.go
@@ -3,6 +3,12 @@ package piscine
 func IsPrime(nb int) bool {
 	var b bool
 	var c float64
+	if nb == 2 {
+		return true
+	}
+	if nb == 1 {
+		return false
+	}
 	for i := 2; i < nb; i++ {
 		c = float64(nb) / float64(i)
 		if c == float64(int(c)) {
