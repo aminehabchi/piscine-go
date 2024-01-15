c7d805c5953ad901e7a7bd7d1913b479fbf4cfe5
ahabchi <you@example.com>
2024-01-15 10:43:16 +0000

    azee

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,19 +1,17 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	n := nb
-	var rusult int = n
-
-	if nb > 1 {
-		n = n - 1
-		rusult = rusult * RecursiveFactorial(n)
-	}
 	if nb == 0 {
 		return 1
 	}
 	if nb < 0 || nb > 31 {
 		return 0
 	}
-
+	n := nb
+	var rusult int = n
+	if nb > 1 {
+		n = n - 1
+		rusult = rusult * RecursiveFactorial(n)
+	}
 	return rusult
 }
