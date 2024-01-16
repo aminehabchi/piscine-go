a0de6f6985f248b8cba366984598fe5dfa695224
Christopher  Fremond <dev@01-edu.org>
2024-01-16 11:22:27 +0000

    Update 'compare.go'

--- compare.go
+++ compare.go
@@ -1,22 +1,12 @@
 package piscine
 
 func Compare(a, b string) int {
-	aa := []rune(a)
-	bb := []rune(b)
-	if aa[0] != bb[0] {
+	if a < b {
+		return 1
+	} else if b < a {
 		return -1
+	} else {
+		return 0
 	}
-
-	if len(a) == len(b) {
-		x := 0
-		for i := 0; i < len(b); i++ {
-			if aa[i] == bb[i] {
-				x = 0
-			} else {
-				return -1
-			}
-		}
-		return x
-	}
-	return 1
 }
+
