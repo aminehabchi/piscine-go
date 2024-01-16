1255f1c5cc68e27d99b7917f4e1605a04efaf7a9
ahabchi <you@example.com>
2024-01-16 13:37:39 +0000

    kjf

---
+++ tolower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ToLower(s string) string {
+	ss := []rune(s)
+
+	for i := 0; i < len(s); i++ {
+		if ss[i] >= 64 && ss[i] <= 90 {
+			ss[i] = rune(ss[i] + 32)
+		}
+	}
+	return string(ss)
+}
