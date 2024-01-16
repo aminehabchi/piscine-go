6f94c5b1f9d511ed21c1bb8bd7e0c497b3d3830b
ahabchi <you@example.com>
2024-01-16 13:42:18 +0000

    kjf

--- tolower.go
+++ tolower.go
@@ -4,7 +4,7 @@ func ToLower(s string) string {
 	ss := []rune(s)
 
 	for i := 0; i < len(s); i++ {
-		if ss[i] >= 64 && ss[i] <= 90 {
+		if ss[i] >= 65 && ss[i] <= 90 {
 			ss[i] = rune(ss[i] + 32)
 		}
 	}
