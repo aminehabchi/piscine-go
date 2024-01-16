c5b0f9c1dc42fa4ac92125177e391c3eaefc1274
ahabchi <you@example.com>
2024-01-16 14:53:21 +0000

    gj

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -6,9 +6,23 @@ func PrintNbrInOrder(n int) {
 	if n == 0 {
 		z01.PrintRune('0')
 	}
-	for n != 0 {
-		s := (n % 10) + '0'
-		z01.PrintRune(rune(s))
+
+	ss := []int{}
+
+	for n > 0 {
+		ss = append(ss, n%10)
 		n = n / 10
+	}
+
+	for i := 0; i < len(ss); i++ {
+		for j := i + 1; j < len(ss); j++ {
+			if ss[i] > ss[j] {
+				ss[i], ss[j] = ss[j], ss[i]
+			}
+		}
+	}
+
+	for i := 0; i < len(ss); i++ {
+		z01.PrintRune(rune(ss[i] + '0'))
 	}
 }
