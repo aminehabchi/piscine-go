eed4208a11558504c23f2afcaff10f938e88725f
ahabchi <you@example.com>
2024-01-19 08:34:17 +0000

    done

---
+++ splitwhitespaces.go
@@ -0,0 +1,17 @@
+package main
+
+func SplitWhiteSpaces(s string) []string {
+	ss := []rune(s)
+	b := ""
+	str := []string{}
+	for i := 0; i < len(ss); i++ {
+		if ss[i] != ' ' {
+			b = b + string(ss[i])
+		}
+		if ss[i] == ' ' {
+			str = append(str, b)
+			b = ""
+		}
+	}
+	return str
+}
