ab8b8bfbd9f7aeee4292d43cb77ac5fd01d78bd6
ahabchi <you@example.com>
2024-01-19 08:15:04 +0000

    done

--- makerange.go
+++ makerange.go
@@ -6,7 +6,7 @@ func MakeRange(min, max int) []int {
 	}
 	arr := make([]int, max-min)
 	arr[0] = min
-	for i := 1; i < len(arr); i++ {
+	for i := 1; i < max-min; i++ {
 		arr[i] += 1
 	}
 	return arr
