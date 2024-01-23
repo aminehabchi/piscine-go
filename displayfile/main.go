de16e0d80f28d2a880647c09a1be8bb87ecacb00
ahabchi <amine.habchi.01@gmail.com>
2024-01-23 08:47:31 +0000

    done

---
+++ displayfile/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	arg := os.Args[1:]
+	if len(arg) == 0 {
+		fmt.Println("File name missing")
+		return
+	}
+	if len(arg) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	name := "./" + arg[0]
+	content, err := ioutil.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", string(content))
+}
