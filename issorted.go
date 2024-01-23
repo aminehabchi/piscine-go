19f395dceef7ea339c8a352431a0824d3af520ba
ahabchi <amine.habchi.01@gmail.com>
2024-01-23 12:40:39 +0000

    d

--- issorted.go
+++ issorted.go
@@ -2,6 +2,7 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	b := true
+
 	for i := 0; i < len(a)-1; i++ {
 		if f(a[i], a[i+1]) < 0 {
 			b = false
@@ -10,7 +11,7 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	if b == false {
 		for j := 0; j < len(a)-1; j++ {
 			if f(a[j], a[j+1]) > 0 {
-				return false
+				b = false
 			}
 		}
 	}
