b55b1608937c6feb216182d76e87c2402757f07a
ahabchi <amine.habchi.01@gmail.com>
2024-01-23 11:11:15 +0000

    done

--- issorted.go
+++ issorted.go
@@ -12,8 +12,8 @@ func f(a, b int) int {
 }
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := len(a) - 1; i > 0; i-- {
-		if f(a[i], a[i-1]) < 0 {
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) > 0 {
 			return false
 		}
 	}
