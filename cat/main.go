511d2808a246e0ed620fce1c910c52f7d5e770ff
ahabchi <amine.habchi.01@gmail.com>
2024-01-23 09:47:32 +0000

    done

--- cat/main.go
+++ cat/main.go
@@ -4,23 +4,34 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	arg := os.Args[1:]
+
 	if len(arg) == 0 {
-		fmt.Println("Hello\nHello")
-		return
+		for {
+			c := ""
+			fmt.Scanln(&c)
+			for i := 0; i < len(c); i++ {
+				z01.PrintRune(rune(c[i]))
+			}
+			z01.PrintRune('\n')
+		}
 	}
+
 	for i := 0; i < len(arg); i++ {
+
 		name := "./" + arg[i]
 		content, err := ioutil.ReadFile(name)
-		if err != nil {
-			fmt.Println("ERROR: open " + name + ": no such file or directory")
-			os.Exit(1)
-			return
+		if err != nil && len(content) == 0 {
+			print("ERROR: open " + name + ": no such file or directory")
+		}
+		for i := 0; i < len(content); i++ {
+			z01.PrintRune(rune(content[i]))
 		}
 
-		fmt.Printf("%s", string(content))
 	}
 }
