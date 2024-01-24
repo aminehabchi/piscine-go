6cdcaad5e7ef6979cc2c269ec72558ef08ce6484
ahabchi <amine.habchi.01@gmail.com>
2024-01-24 17:08:16 +0100

    done

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -4,7 +4,10 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	a := map[string]int{"Burger": 0, "Water": 0, "Carrot": 0, "Coffee": 0, "Chips": 0}
 	b := ""
 	for i := 0; i < len(str); i++ {
-		if str[i] == ' ' {
+		if str[i] != ' ' {
+			b = b + string(str[i])
+		}
+		if str[i] == ' ' || i == len(str)-1 {
 			switch {
 			case b == "Burger":
 				a["Burger"]++
@@ -18,9 +21,8 @@ func ShoppingSummaryCounter(str string) map[string]int {
 				a["Chips"]++
 			}
 			b = ""
-		} else {
-			b = b + string(str[i])
 		}
+
 	}
 
 	return a
