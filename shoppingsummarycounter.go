7ec482b3fb93173a0069e251cb97f28d109198a8
ahabchi <amine.habchi.01@gmail.com>
2024-01-24 17:49:17 +0100

    done

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,44 +1,14 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	a := map[string]int{}
-	b := ""
-	bb := []string{}
-	for i := 0; i < len(str); i++ {
-		if str[i] != ' ' {
-			b = b + string(str[i])
-		}
-		if str[i] == ' ' || i == len(str)-1 {
-			bb = append(bb, b)
-			b = ""
+	s := Split(str, " ")
+	a := make(map[string]int, len(s))
+	for _, ele := range s {
+		if _, ok := a[ele]; ok {
+			a[ele]++
+		} else {
+			a[ele] = 1
 		}
 	}
-	for i := 0; i < len(bb); i++ {
-		for j := 0; j < len(bb); j++ {
-			if bb[i] > bb[j] {
-				bb[i], bb[j] = bb[j], bb[i]
-			}
-		}
-	}
-
-	for i := 1; i < len(bb); i++ {
-		if bb[i] != bb[i-1] {
-			a[bb[i]] = 0
-		}
-	}
-
-	for i := 0; i < len(bb); i++ {
-		count := 0
-		for j := 0; j < len(bb); j++ {
-			if bb[i] == bb[j] {
-				count++
-			}
-		}
-		if bb[i] == "" && len(bb) == 1 {
-			count++
-		}
-		a[bb[i]] = count
-	}
-
 	return a
 }
