d1afcae113ee17193193a94c00b2ab3cf71f79b5
ahabchi <amine.habchi.01@gmail.com>
2024-01-24 17:35:24 +0100

    done

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,28 +1,40 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	a := map[string]int{"Burger": 0, "Water": 0, "Carrot": 0, "Coffee": 0, "Chips": 0}
+	a := map[string]int{}
 	b := ""
+	bb := []string{}
 	for i := 0; i < len(str); i++ {
 		if str[i] != ' ' {
 			b = b + string(str[i])
 		}
 		if str[i] == ' ' || i == len(str)-1 {
-			switch {
-			case b == "Burger":
-				a["Burger"]++
-			case b == "Water":
-				a["Water"]++
-			case b == "Carrot":
-				a["Carrot"]++
-			case b == "Coffee":
-				a["Coffee"]++
-			case b == "Chips":
-				a["Chips"]++
-			}
+			bb = append(bb, b)
 			b = ""
 		}
+	}
+	for i := 0; i < len(bb); i++ {
+		for j := 0; j < len(bb); j++ {
+			if bb[i] > bb[j] {
+				bb[i], bb[j] = bb[j], bb[i]
+			}
+		}
+	}
 
+	for i := 1; i < len(bb); i++ {
+		if bb[i] != bb[i-1] {
+			a[bb[i]] = 0
+		}
+	}
+
+	for i := 0; i < len(bb); i++ {
+		count := 0
+		for j := 0; j < len(bb); j++ {
+			if bb[i] == bb[j] {
+				count++
+			}
+		}
+		a[bb[i]] = count
 	}
 
 	return a
