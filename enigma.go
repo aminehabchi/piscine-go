0817444af691bb9c6cb8593a8552dad9d8f20591
ahabchi <amine.habchi.01@gmail.com>
2024-01-24 23:01:53 +0100

    done

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	aa := ***a
+	cc := *******c
+	dd := ****d
+	bb := *b
+
+	***a = bb
+	*******c = aa
+	****d = cc
+	*b = dd
+}
