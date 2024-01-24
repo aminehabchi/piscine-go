dcb77b5b366fbd7491e4d010c3dbb7ef870677bc
ahabchi <amine.habchi.01@gmail.com>
2024-01-24 12:11:56 +0100

    done

--- rot14.go
+++ rot14.go
@@ -1,29 +1,30 @@
 package piscine
+
 func Rot14(s string) string {
-  ss:=[]byte(s)
-  b:=""
-  a:=0
-  for i:=0;i<len(ss);i++{
-    if ss[i]>=65 && ss[i]<=90{
-      
-      if ss[i]>=65 && ss[i]<=76{
-        ss[i]=ss[i]+14
-      }else{
-        a=int(ss[i]-77)+65
-        ss[i]=byte(a)
-      }
-      
-    }else if ss[i]>=97 && ss[i]<=122{
-      
-      if ss[i]>=97 && ss[i]<=108{
-        ss[i]=ss[i]+14
-      }else{
-        a=int(ss[i]-109)+97
-        ss[i]=byte(a)
-      }
-      
-    }
-     b=b+string(ss[i])
-  }
-  return b
+	ss := []byte(s)
+	b := ""
+	a := 0
+	for i := 0; i < len(ss); i++ {
+		if ss[i] >= 65 && ss[i] <= 90 {
+
+			if ss[i] >= 65 && ss[i] <= 76 {
+				ss[i] = ss[i] + 14
+			} else {
+				a = int(ss[i]-77) + 65
+				ss[i] = byte(a)
+			}
+
+		} else if ss[i] >= 97 && ss[i] <= 122 {
+
+			if ss[i] >= 97 && ss[i] <= 108 {
+				ss[i] = ss[i] + 14
+			} else {
+				a = int(ss[i]-109) + 97
+				ss[i] = byte(a)
+			}
+
+		}
+		b = b + string(ss[i])
+	}
+	return b
 }
