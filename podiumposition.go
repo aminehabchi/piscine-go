e97e4cf25efa00082f515edb2c6b4dca79704d17
ahabchi <amine.habchi.01@gmail.com>
2024-01-25 03:41:12 +0100

    done

---
+++ podiumposition.go
@@ -0,0 +1,14 @@
+package piscine
+
+func PodiumPosition(podium [][]string) [][]string {
+	for i := 0; i < len(podium); i++ {
+		for j := 0; j < len(podium); j++ {
+			if i != j {
+				if podium[i][0] < podium[j][0] {
+					podium[i], podium[j] = podium[j], podium[i]
+				}
+			}
+		}
+	}
+	return podium
+}
