c4363f15303556e805175928d942e625d62f9401
ahabchi <amine.habchi.01@gmail.com>
2024-01-25 02:05:01 +0100

    done

--- comcheck/main.go
+++ comcheck/main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/01-edu/z01"
 )
 
 func main() {
 	arg := os.Args[1:]
 	for i := 0; i < len(arg); i++ {
 		if arg[i] == "galaxy" || arg[i] == "galaxy 01" || arg[i] == "01" {
-			print("Alert!!!")
-			z01.PrintRune('A')
-			z01.PrintRune('l')
-			z01.PrintRune('e')
-			z01.PrintRune('r')
-			z01.PrintRune('t')
-			z01.PrintRune('!')
-			z01.PrintRune('!')
-			z01.PrintRune('!')
+			fmt.Println("Alert!!!")
 			break
 		}
 	}
