1443f94006ddd4bf7d8a68fd68874285f5271459
ahabchi <amine.habchi.01@gmail.com>
2024-01-26 10:22:09 +0100

    done

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,16 +1,17 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	m := map[string]int{}
-	s := ""
-	for index, char := range str {
-		if char != ' ' {
-			s = s + string(byte(char))
-		} else if char == ' ' || index == len(str)-1 {
-			m[s] = m[s] + 1
-			s = ""
+	list := make(map[string]int)
+	var items string
+	for _, e := range str {
+		if e == 32 {
+			list[items] += 1
+			items = ""
+		} else if e != 32 {
+			items += string(byte(e))
 		}
 	}
+	list[items] += 1
 
-	return m
+	return list
 }
