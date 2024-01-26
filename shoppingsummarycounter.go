120d69e8a7e722fb4e7777932bbcf1a4c6c97516
ahabchi <amine.habchi.01@gmail.com>
2024-01-26 10:14:09 +0100

    done

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -3,8 +3,8 @@ package piscine
 func ShoppingSummaryCounter(str string) map[string]int {
 	m := map[string]int{}
 	s := ""
-	for _, char := range str {
-		if char == ' ' || char == rune(str[len(str)-1]) {
+	for index, char := range str {
+		if char == ' ' || index == len(str)-1 {
 			m[s] = m[s] + 1
 			s = ""
 		} else {
