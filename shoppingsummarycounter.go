7374e78783fe421c7b0f53cd820598ef632c8d90
ahabchi <amine.habchi.01@gmail.com>
2024-01-26 10:16:33 +0100

    done

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -4,12 +4,14 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	m := map[string]int{}
 	s := ""
 	for index, char := range str {
+		if char != ' ' {
+			s = s + string(char)
+		}
 		if char == ' ' || index == len(str)-1 {
 			m[s] = m[s] + 1
 			s = ""
-		} else {
-			s = s + string(char)
 		}
+
 	}
 	return m
 }
