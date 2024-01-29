61f87fa5045a7a5e405ed9d1a0933c030150cf52
Christopher  Fremond <dev@01-edu.org>
2024-01-29 11:56:35 +0000

    Update 'listsize.go'

--- listsize.go
+++ listsize.go
@@ -1,4 +1,23 @@
 package piscine
+func ListPushFront(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+
+	if l.Tail == nil {
+		l.Tail = n
+	} else {
+		n.Next = l.Head
+	}
+	l.Head = n
+}
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
 
 
 func ListSize(l *List) int {
