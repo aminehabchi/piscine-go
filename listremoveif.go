91d761eb1c20f4562e4c9b5487320c9466df97db
amine habchi <ahabchi@noreply.learn.zone01oujda.ma>
2024-01-30 12:19:09 +0000

    Update 'listremoveif.go'

--- listremoveif.go
+++ listremoveif.go
@@ -1,11 +1,9 @@
 package piscine
 
-
 func ListRemoveIf(l *List, data_ref interface{}) {
 	c := l.Head
 	var p *NodeL
 	for c != nil {
-
 		if c.Data == data_ref {
 			if p == nil {
 				l.Head = c.Next
@@ -16,6 +14,5 @@ func ListRemoveIf(l *List, data_ref interface{}) {
 			p = c
 		}
 		c = c.Next
-
 	}
 }
