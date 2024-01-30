05028490b3e458ab5cb253f515c3d85bba63a32b
ahabchi <amine.habchi.01@gmail.com>
2024-01-30 10:34:12 +0100

    done

---
+++ listremoveif.go
@@ -0,0 +1,20 @@
+package piscine
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+	c := l.Head
+	var p *NodeL
+	for c != nil {
+
+		if c.Data == data_ref {
+			if p == nil {
+				l.Head = c.Next
+			} else {
+				p.Next = c.Next
+			}
+		} else {
+			p = c
+		}
+		c = c.Next
+
+	}
+}
