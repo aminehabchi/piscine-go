6d2cb8bda74abf477ea7683c40e6d0fb42cfe516
ahabchi <amine.habchi.01@gmail.com>
2024-01-30 10:56:09 +0100

    done

---
+++ listmerge.go
@@ -0,0 +1,22 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListMerge(l1 *List, l2 *List) {
+	c := l1.Head
+	for c != nil {
+		if c.Next == nil {
+			c.Next = l2.Head
+			break
+		}
+		c = c.Next
+	}
+}
