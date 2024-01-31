8f63f159ad8bb08616754b176fae962df1f25940
ahabchi <amine.habchi.01@gmail.com>
2024-01-31 10:26:36 +0100

    done

---
+++ btreeapplypostorder.go
@@ -0,0 +1,26 @@
+package piscine
+
+func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	if root.Left != nil {
+		if root.Left.Data != "" {
+			f(root.Left.Data)
+			root.Left.Data = ""
+			BTreeApplyPostorder(root.Right, f)
+		}
+	}
+	if root.Right != nil {
+		if root.Right.Data != "" {
+			f(root.Right.Data)
+			root.Right.Data = ""
+			BTreeApplyPostorder(root.Left, f)
+		}
+	}
+	if root.Data != "" {
+		f(root.Data)
+		root.Data = ""
+	}
+}
