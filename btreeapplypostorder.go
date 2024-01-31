5f0fd9763dc6506eb8c0b8767c902192397cc9a6
ahabchi <amine.habchi.01@gmail.com>
2024-01-31 11:59:58 +0100

    done

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -4,24 +4,7 @@ func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	if root.Data != "" {
-		f(root.Data)
-		root.Data = ""
-	}
-
-	if root.Right != nil {
-		if root.Right.Data != "" {
-			f(root.Right.Data)
-			root.Right.Data = ""
-			BTreeApplyPostorder(root.Right, f)
-		}
-	}
-	if root.Left != nil {
-		if root.Left.Data != "" {
-			f(root.Left.Data)
-			root.Left.Data = ""
-			BTreeApplyPostorder(root.Left, f)
-		}
-	}
-
+	BTreeApplyPostorder(root.Left, f)
+	BTreeApplyPostorder(root.Right, f)
+	f(root.Data)
 }
