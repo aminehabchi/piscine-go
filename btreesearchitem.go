b2c307f53aee07dbfdfd597a18ccb004bf712184
ahabchi <amine.habchi.01@gmail.com>
2024-01-31 12:42:25 +0100

    done

---
+++ btreesearchitem.go
@@ -0,0 +1,19 @@
+package piscine
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root.Left != nil {
+		if elem != root.Left.Data {
+			BTreeSearchItem(root.Left, elem)
+		} else {
+			return root.Left
+		}
+	}
+	if root.Right != nil {
+		if elem != root.Right.Data {
+			BTreeSearchItem(root.Right, elem)
+		} else {
+			return root.Right
+		}
+	}
+	return root
+}
