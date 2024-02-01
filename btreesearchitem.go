4c20d34069848f3eed74f2bbdc316b353bbd370f
ahabchi <amine.habchi.01@gmail.com>
2024-02-01 09:21:14 +0100

    done

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,27 +1,71 @@
-package piscine
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root.Data == elem {
+	if root == nil {
+		return nil
+	}
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	} else if elem > root.Data {
+		return BTreeSearchItem(root.Right, elem)
+	} else {
 		return root
 	}
-	if root.Left != nil {
-		if elem != root.Left.Data {
-			BTreeSearchItem(root.Left, elem)
-		} else {
-			return root.Left
-		}
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
 	}
-	if root.Right != nil {
-		if elem != root.Right.Data {
-			BTreeSearchItem(root.Right, elem)
-		} else {
-			return root.Right
-		}
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
 	}
 
-	root.Parent = nil
-	root.Data = elem
-	root.Left = nil
-	root.Right = nil
 	return root
+}
+
+func main() {
+	root := &TreeNode{Data: "4"}
+	BTreeInsertData(root, "1")
+	BTreeInsertData(root, "7")
+	BTreeInsertData(root, "5")
+	selected := BTreeSearchItem(root, "7")
+	fmt.Print("Item selected -> ")
+	if selected != nil {
+		fmt.Println(selected.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Parent of selected item -> ")
+	if selected.Parent != nil {
+		fmt.Println(selected.Parent.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Left child of selected item -> ")
+	if selected.Left != nil {
+		fmt.Println(selected.Left.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Right child of selected item -> ")
+	if selected.Right != nil {
+		fmt.Println(selected.Right.Data)
+	} else {
+		fmt.Println("nil")
+	}
 }
